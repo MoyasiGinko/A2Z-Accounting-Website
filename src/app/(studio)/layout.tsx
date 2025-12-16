8441925fc68891da0cd6f7771ac8564d0d0299d8
moyasiginko <mrsiyam61@gmail.com>
2025-12-16 19:33:57 +0600

    restructure-app

---
+++ src/app/(studio)/layout.tsx
@@ -0,0 +1,20 @@
+// import type { Metadata } from "next";
+import "./globals.css";
+// import AppShell from "@/components/AppShell";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en-US" className="no-js">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body className="">{children}</body>
+    </html>
+  );
+}
