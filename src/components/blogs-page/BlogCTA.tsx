5995fc367395b4b503713f50ba7219cc4484b4e2
moyasiginko <mrsiyam61@gmail.com>
2025-12-22 15:31:29 +0600

    Update BlogCTA.tsx

--- src/components/blogs-page/BlogCTA.tsx
+++ src/components/blogs-page/BlogCTA.tsx
@@ -3,18 +3,18 @@ import React from "react";
 const BlogCTA: React.FC = () => {
   return (
     <section
-      className="bg-primary text-white py-12 px-4 text-center
-    max-w-7xl mx-auto my-16 rounded-lg shadow-lg"
+      className="bg-transparent text-primary py-12 px-4 text-center
+    max-w-7xl mx-auto my-16"
     >
       <div className="max-w-2xl mx-auto">
-        <h2 className="text-3xl font-bold mb-4">
+        <h2 className="text-3xl font-medium mb-4 font-serif">
           Stay Updated with Our Latest Blogs
         </h2>
-        <p className="text-lg mb-6">
+        <p className="text-lg text-primary/70 mb-6 font-sans">
           Find expert tips on accounting, finance, and business growth. Get in
           touch with us for working together on your accounting needs.
         </p>
-        <button className="bg-white text-primary px-6 py-3 rounded-lg font-semibold hover:bg-gray-100 transition">
+        <button className="bg-secondary text-primary/90 px-6 py-3 rounded-lg font-medium hover:text-white hover:bg-primary transition">
           Get in Touch
         </button>
       </div>
